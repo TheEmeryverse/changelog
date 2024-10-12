3b174b5a70292c8126bd27943e952c0921994256
The Emeryverse <server@emeryverse.com>
2024-10-11 22:51:04 -0500

    changed to dist folder

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-cd /home/containerConfigs/changelog
+cd /home/containerConfigs/changelog/
 
 git pull
 docker stop changelog
 docker rm changelog
+cd dist
 docker build -t html-site .
 docker run -d --name=changelog -p 7070:80 html-site
